cffdb1997ce06f009171565b837372495a93d747
ciross1 <oricsantamarina@gmail.com>
2025-08-26 18:50:38 +0200

    save

--- src/main/java/com/myself/Enotes/EnotesApplication.java
+++ src/main/java/com/myself/Enotes/EnotesApplication.java
@@ -8,7 +8,7 @@ public class EnotesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnotesApplication.class, args);
-		System.out.println("hellooooo");
+		System.out.println("helloooooo");
 	}
 
 }
